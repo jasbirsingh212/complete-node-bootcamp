9364ede765246e5a0d9d15dcd377603e742656ed
Jasbir Singh <jasbir212singh@gmail.com>
2024-01-04 13:25:05 +0530

    writing file with fs.writeFileSync methode

--- 1-node-farm/practise/index.js
+++ 1-node-farm/practise/index.js
@@ -2,3 +2,12 @@ const fs = require("fs");
 
 const readText = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(readText);
+
+const arr = ["Jasbir", "Singh", "25", "frontend-development"];
+const writeText = `My name is ${arr[0]} ${arr[1]}.
+ My age is ${arr[2]}.
+ I want to become a ${arr[3]}.
+`;
+
+fs.writeFileSync("./txt/output.txt", writeText);
+console.log("file is written");
