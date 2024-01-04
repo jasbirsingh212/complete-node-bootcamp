8967bb7542d9d381c494aaee9727d46c224f9efd
Jasbir Singh <jasbir212singh@gmail.com>
2024-01-04 14:31:26 +0530

    reading and writing file asynchronously and call-back hell

--- 1-node-farm/practise/index.js
+++ 1-node-farm/practise/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// Blocking code, synchronous way---------------------------------------
+/*
 const readText = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(readText);
 
@@ -9,8 +11,36 @@ const writeText = `My name is ${arr[0]} ${arr[1]}.
  I want to become a ${arr[3]}.
 `;
 
-// fs.writeFileSync("./txt/output.txt", writeText);
-// console.log("file is written");
+fs.writeFileSync("./txt/output.txt", writeText);
+console.log("file is written");
 
 fs.writeFileSync("./txt/output.txt", "I Love Coding!");
 console.log("file is written");
+*/
+//---------------------------------------------------------------------------
+
+//Non-blocking code, asynchronous way ----------------------------------------------
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   if (err) return "Error from callback 1";
+
+//   console.log(data);
+// });
+
+fs.readFile("./txt/startt.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error from callback 1");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    if (err) return console.log("Error from callback 2");
+
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      if (err) return console.log("Error from callback 3");
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File Has Been Written!");
+      });
+    });
+  });
+});
+
+console.log("Reading file ....");
